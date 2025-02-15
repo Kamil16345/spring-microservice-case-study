ea2880d541b6653210e51550937f23f268a43ffc
Kamil <KamilArkit@interia.pl>
2025-02-15 16:40:53 +0100

    Updated UserRepository with new method and Query.

--- src/main/java/com/betacom/demo/repository/UserRepository.java
+++ src/main/java/com/betacom/demo/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.betacom.demo.repository;
 
 import com.betacom.demo.model.security.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
 public interface UserRepository extends CrudRepository<User, UUID> {
-
+    @Query("SELECT u FROM User u WHERE u.login = :login")
+    User findUserByLogin(String login);
 }

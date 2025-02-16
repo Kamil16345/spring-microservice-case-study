976d2330a50b8c59e164e413308c0e4fdc836779
Kamil <KamilArkit@interia.pl>
2025-02-16 11:51:59 +0100

    Added response statuses to AuthenticationController

--- src/main/java/com/betacom/demo/controller/AuthenticationController.java
+++ src/main/java/com/betacom/demo/controller/AuthenticationController.java
@@ -5,11 +5,9 @@ import com.betacom.demo.dto.security.AuthenticationResponse;
 import com.betacom.demo.dto.security.RegisterRequest;
 import com.betacom.demo.service.security.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,11 +16,13 @@ public class AuthenticationController {
     private final AuthenticationService authService;
 
     @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
 
     @GetMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<AuthenticationResponse> login(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
